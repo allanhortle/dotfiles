ee5b4d8a18820603bc33645eb4ea037167aba5ca
Allan Hortle <allanhortle@gmail.com>
2023-02-11 14:53:50 +1100

    Fixes after setting up new laptop

--- apps.sh
+++ apps.sh
@@ -4,8 +4,9 @@ brew update
 brew install --cask sol
 brew install --cask 1password
 brew install --cask google-chrome
+brew install --cask firefox
+brew install --cask dropbox
 brew install --cask alacritty
 brew install --cask slack
 brew install --cask spotify
-brew install --cask transmit
 brew install --cask phoenix
