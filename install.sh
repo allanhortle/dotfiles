f9a43fb07b0918f4663984d6613ecf58cf5397f8
Allan Hortle <allanhortle@gmail.com>
2024-04-23 12:13:14 +1000

    Move install into a Brewfile

--- install.sh
+++ install.sh
@@ -2,32 +2,8 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval $(/opt/homebrew/bin/brew shellenv)
 
-brew update
-
-brew install gh
-brew install fnm
-brew install fzf
-brew install git
-brew install htop
-brew install neovim
-brew install node
-brew install qlmarkdown
-brew install qlstephen
-brew install ripgrep
-brew install saulpw/vd/visidata
-brew install stow
-brew install tig
-brew install tmux
-brew install vim
-brew install yarn
-brew install zoxide
-
+brew bundle 
 brew cleanup
-
 stow dotfiles --no-folding
-
 source ~/.zshrc
-
-# Python rubbish for nvim snippets
-pip3 install pynvim
 
