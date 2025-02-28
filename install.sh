2175d9abff98726b1730fbacf30a8f114bba5fad
Allan Hortle <allanhortle@gmail.com>
2025-02-28 19:22:21 +1100

    Don't re-download brew if it's there already

--- install.sh
+++ install.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env zsh
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval $(/opt/homebrew/bin/brew shellenv)
+
+# Install brew
+which -s brew
+if [[ $? != 0 ]] ; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval $(/opt/homebrew/bin/brew shellenv)
+else
+    brew update
+fi
 
 brew bundle 
 stow dotfiles --no-folding
 source ~/.zshrc
+fnm install --latest
 
+# Hide everthing from the dock
 defaults write "com.apple.dock" "persistent-apps" -array
+
+# turn off natural scroll
+defaults -currentHost write -g com.apple.swipescrolldirection -bool false
 
 # Remap capslock to control on the default keyboard
 defaults -currentHost write -g com.apple.keyboard.modifiermapping '({                                                                
